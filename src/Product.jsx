25e114426ea9cda0848297d3a2590815e854ce40
Jashanjot Singh <jsjsnarang6@gmail.com>
2024-08-29 00:06:33 +0530

    used props,rendering arrays ,added dynamic component styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,9 +1,15 @@
 import "./Product.css"
-function Product() {
+function Product({ title, price, features }) {
+    const list = features.map((features) => <li>{features}</li>)
+    let style = { backgroundColor: price>30000?"skyblue":"" };;
+
+
     return (
-        <div className="Product">
-            <h3>Product title</h3>
-            <h5>Product Description</h5>
+        <div className="Product" style={style}>
+            <h3>{title}</h3>
+            <h5>Price:{price}</h5>
+            <p>{list}</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : null}
         </div>
     );
 }
--- src/Producttab.jsx
+++ src/Producttab.jsx
@@ -1,11 +1,12 @@
 
 import Product from "./Product.jsx"
 function Producttab() {
+    let options = ["durable", "fast", "hi-tech"];
     return (
         <>
-            <Product />
-            <Product />
-            <Product />
+            <Product title="phone" price={20000} features={options} />
+            <Product title="laptop" price={40000}features={options} />
+            <Product title="charger" price={2000} features={options} />
         </>
     )
 }
