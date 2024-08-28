eaed62275ed8defe1940438b79f028856a6fc4a0
Jashanjot Singh <jsjsnarang6@gmail.com>
2024-08-28 21:33:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Title from "./Title.jsx"
+
+function App() {
+  return (
+    <div>
+      <h1>this is my app component</h1>
+      <p>inside component we have :</p>
+      <Title />
+    </div>
+
+  );
+
+
+}
+
+export default App
---
+++ src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+    return <h1> this is my title </h1>;
+}
+export default Title;
+// export { Title ,add};
